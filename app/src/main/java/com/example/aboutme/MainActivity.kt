4908e6451f6482c03627308c027caad8dd4d92ca
The Lood <unilood@gmail.com>
2022-06-17 12:24:07 +1200

    Complete

---
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fun addNickname(view: View) {
+            val editText = findViewById<EditText>(R.id.name_edit)
+            val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+            nicknameTextView.text = editText.text
+            editText.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameTextView.visibility = View.VISIBLE
+
+            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener() {
+            updateNickname(it)
+        }
+    }
+
+    private fun updateNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.name_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        editText.visibility = View.VISIBLE
+        view.visibility = View.VISIBLE
+        nicknameTextView.visibility = View.GONE
+
+        editText.requestFocus()
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(editText, 0)
+
+    }
+}
